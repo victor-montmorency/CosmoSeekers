f6137e397148ab5112c559fbad95bea5b1d0e2cf
victor-montmorency <67494565+victor-montmorency@users.noreply.github.com>
2025-01-24 01:19:01 -0300

    Initial Commit

---
+++ CosmoSeekers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CosmoSeekers
+//
+//  Created by victor mont-morency on 24/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
